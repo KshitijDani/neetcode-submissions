70f21cba96c6d136b32e36e7e40e83f5fccdfb86
Kshitij Dani <kdani500@gmail.com>
2026-07-27 12:00:58 -0700

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.py
@@ -0,0 +1,37 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+
+        l=0
+        maxHeight = 0
+        prefixHeights = []
+
+        while l<len(height):
+            maxHeight=max(maxHeight, height[l])
+            prefixHeights.append(maxHeight)
+            l+=1
+        print(prefixHeights)
+        
+        r=len(height)-1
+        maxHeight = 0
+        suffixHeights = []
+
+        while r>=0:
+            maxHeight=max(maxHeight, height[r])
+            suffixHeights.append(maxHeight)
+            r-=1
+
+        # Align suffix values with the original indices
+        suffixHeights.reverse()
+        print(suffixHeights)
+
+
+        maxArea = 0
+        for i in range(len(height)):
+            maxArea += min(prefixHeights[i], suffixHeights[i]) - height[i]
+            print(i, maxArea)
+
+        return maxArea
+    
+            
+
+        
