4b3d98f197c14309440acebd660ea107f3fe46d5
Kshitij Dani <kdani500@gmail.com>
2026-07-28 13:07:37 -0700

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        days = [0]*len(temperatures)
+        stack = []
+
+        for i in range(len(temperatures)):
+            while stack and temperatures[i]>temperatures[stack[-1]]:
+                # essentially stack[-1] is the stack.top() and the latest high temperature's index
+                # so if we do i - stack[-1], we'll get to know the total distance in days between the highest temperature after stack[-1]
+                days[stack[-1]] = i - stack[-1]
+                stack.pop()
+            # we can't push just the temperature because then we won't know what index that temp appeared at
+            # so instead push the index where that temperature appeared
+            stack.append(i)
+        return days
+
+        
