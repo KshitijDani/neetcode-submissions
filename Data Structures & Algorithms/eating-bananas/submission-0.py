22ee57e7be2004b30b6f42d68926fd535fe38ebd
Kshitij Dani <kdani500@gmail.com>
2026-07-29 13:39:28 -0700

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,23 @@
+from math import ceil
+
+class Solution:
+    def calcRate(self, piles: List[int], rate:int):
+        hours = 0
+        for pile in piles:
+            hours+=ceil(pile/rate)
+
+        return hours
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l,r=1,max(piles)
+
+        while l<=r:
+            rate = l + (r-l)//2
+            hours = self.calcRate(piles, rate)
+            if hours <= h:
+                #if the koko is eating too fast, then the overall rate needs to lower than the current value
+                r=rate-1
+            else:
+                l=rate+1
+
+        return l
