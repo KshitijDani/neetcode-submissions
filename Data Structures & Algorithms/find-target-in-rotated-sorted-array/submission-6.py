9fcadb08f258b1ed9ca775ea4d5ae61e491c014f
Kshitij Dani <kdani500@gmail.com>
2026-08-06 13:22:10 -0700

    Add: find-target-in-rotated-sorted-array - submission-6

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-6.py
@@ -0,0 +1,39 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        res=nums[0]
+        cut = 0
+        l=0
+        r=len(nums)-1
+
+        # first find the index of the cut, which is basically the smallest cut
+        while l<r:
+            mid = (l+r)//2
+            if nums[mid]>nums[r]:
+                l=mid+1
+            else:
+                r=mid
+        cut = l
+        # then use the cut to determine whether the target is in the left or right sorted arrays
+
+        # need to reinitialize these variables
+        l = 0
+        r = len(nums)-1
+        
+        if nums[cut]<=target and target <=nums[r]:
+            l=cut
+        else:
+            r=cut-1
+
+        # do binary search in the relevant sorted array
+        while l<=r:
+            mid = (l+r)//2
+            if nums[mid]==target:
+                return mid
+
+            if nums[mid]<target:
+                l=mid+1
+            else:
+                r=mid-1
+
+        return -1
+        
